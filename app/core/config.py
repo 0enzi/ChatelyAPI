ab4485ddb12d70017d0a78c8b767ebb679c3d3e5
Elvis Chege <elvischege@icloud.com>
2022-06-14 05:48:18 +0200

    Boilerplate code

--- app/core/config.py
+++ app/core/config.py
@@ -5,9 +5,9 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
-    PROJECT_NAME: str = "FastAPI"
+    PROJECT_NAME: str = "ChatelyAPI"
     SECRET_KEY: str = secrets.token_urlsafe(32)
-    SQLALCHEMY_DATABASE_URI: str = "postgresql://localhost:5432/fastapi_db"
+    SQLALCHEMY_DATABASE_URI: str = "postgresql://localhost:5432/chately_db"
 
     class Config:
         env_file = ".env"
