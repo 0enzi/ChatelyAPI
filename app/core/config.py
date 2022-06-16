c898b46ae9b54d2c68351f9e75938f1feefc4b9d
Elvis Chege <elvischege@icloud.com>
2022-06-16 06:11:42 +0200

    Securty

--- app/core/config.py
+++ app/core/config.py
@@ -1,13 +1,17 @@
 import secrets
-
 from pydantic import BaseSettings
 
+# Security
+import os
+from dotenv import load_dotenv
+load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "ChatelyAPI"
     SECRET_KEY: str = secrets.token_urlsafe(32)
-    SQLALCHEMY_DATABASE_URI: str = "postgresql://localhost:5432/chately"
+    SQLALCHEMY_DATABASE_URI: str = DATABASE_URL
 
     class Config:
         env_file = ".env"
--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ import uvicorn
 
 from fastapi import FastAPI
 
-
 from app.api.v1 import api_router
 from app.core import settings
 from app.models import product
 from app.database import session
+
 app = FastAPI(title=settings.PROJECT_NAME)
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
