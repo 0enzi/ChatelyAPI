145c339f0e43e19527f740a9e881e882eabb3f7f
Elvis Chege <elvischege@icloud.com>
2022-06-17 16:16:11 +0200

    Message and User Model

--- app/schemas/message.py
+++ app/schemas/message.py
@@ -2,4 +2,9 @@ from pydantic import BaseModel
 
 
 class Message(BaseModel):
+    id: int
+    from_id: int
+    to_id: int
+    msg: str
+    datetime: str
     message: str
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -4,17 +4,22 @@ from pydantic import BaseModel, EmailStr
 
 
 class UserCreate(BaseModel):
-    # id: int
+    id: int
+    username: str
     email: EmailStr
     password: str
-    # created_at: datetime
-    # active: Optional[bool] = True
+    created_at: datetime
+    active: Optional[bool] = True
+    status: bool
+    profile: str
 
 
 class UserOut(BaseModel):
     id: int
+    username: str
     email: EmailStr
     created_at: datetime
+    profile: str
 
     class Config:
         orm_mode = True
