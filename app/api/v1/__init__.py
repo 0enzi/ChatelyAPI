ca3d7961d8e38ec0a4202ebb02d8e41399ca0f01
Elvis Chege <elvischege@icloud.com>
2022-06-17 11:25:41 +0200

    auth implemented

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.v1 import products
+from app.api.v1 import products, auth, products, user
 
 api_router = APIRouter()
 api_router.include_router(products.router, prefix="/products", tags=["products"])
+api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+api_router.include_router(user.router, prefix="/users", tags=["Users"])
