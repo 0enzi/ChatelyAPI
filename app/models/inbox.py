d771143575dca005ee50b17810658e2dee4ae79b
Elvis Chege <elvischege@icloud.com>
2022-06-19 06:49:47 +0200

    Models have thier own files

---
+++ app/models/inbox.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Boolean, Column, DateTime, Integer, String, Float
+from app.database.base_class import Base
+
+
+class Inbox(Base):
+    __tablename__ = "inbox"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer)
+    sender_id = Column(Integer)
+    unread_count = Column(Integer)
+    last_message = Column(String)
+    inbox_hash = Column(String)
