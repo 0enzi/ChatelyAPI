8a0ff6012e4c9bf5a11085c1f7f0ef3a095da70d
Elvis Chege <elvischege@icloud.com>
2022-06-20 05:28:01 +0200

    refactor code

--- app/api/v1/chat.py
+++ app/api/v1/chat.py
@@ -1,20 +1,30 @@
 
-# from fastapi import APIRouter, Depends, HTTPException, status
-# from sqlalchemy.orm import Session
+from typing import List, Any
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
 
-# from app import schemas, crud
-
-# from app.schemas import product as product_schema
-# from app.schemas import message as message_schema
-# from app.api.deps import get_db
-
-# router = APIRouter()
+from app import schemas, crud
 
 
-# @router.get("", response_model=List[product_schema.ProductResponse])
-# def read_products(db: Session = Depends(get_db), skip: int = 0, limit: int = 100) -> Any:
-#     """
-#     Retrieve all products.
-#     """
-#     products = crud.product.get_multi(db, skip=skip, limit=limit)
-#     return products
+from app.schemas import message as message_schema
+from app.api.deps import get_db
+
+router = APIRouter()
+
+
+@router.get("", response_model=List[message_schema.Message])
+def read_messages(db: Session = Depends(get_db), skip: int = 0, limit: int = 100) -> Any:
+    """
+    Retrieve all messages.
+    """
+    print(db)
+    messages = crud.messages.get_multi(db, skip=skip, limit=limit)
+    return messages
+
+@router.post("", response_model=message_schema.Message)
+def create_message(*, db: Session = Depends(get_db), message_in: message_schema.Message) -> Any:
+    """
+    Create new messages.
+    """
+    message = crud.message.create(db, obj_in=message_in)
+    return message
