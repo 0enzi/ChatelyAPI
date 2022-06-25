5911eb4e3852385a2742a87710be6860f12cc944
Elvis Chege <elvischege@icloud.com>
2022-06-25 05:29:19 +0200

    Auto-Increment Model

--- app/models/inbox.py
+++ app/models/inbox.py
@@ -4,9 +4,9 @@ from app.database.base_class import Base
 
 class Inbox(Base):
     __tablename__ = "inbox"
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     user_id = Column(Integer)
     sender_id = Column(Integer)
     unread_count = Column(Integer)
     last_message = Column(String)
-    inbox_hash = Column(String)
+    inbox_hash = Column(String) # 23-32 sender id 
