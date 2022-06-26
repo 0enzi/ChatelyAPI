8bce277c3d4679ad34a5037e5a3ea72e427b5d0d
Elvis Chege <elvischege@icloud.com>
2022-06-26 05:01:05 +0200

    Implement sockets

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.v1 import chat, auth, user
+from app.api.v1 import chat, auth, user,sockets 
 
 api_router = APIRouter()
 # api_router.include_router(products.router, prefix="/products", tags=["products"])
 api_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 api_router.include_router(user.router, prefix="/users", tags=["Users"])
 api_router.include_router(chat.router, prefix="/chat", tags=["Chat"])
+api_router.include_router(sockets.router, prefix="/ws", tags=["sockets"])
+
