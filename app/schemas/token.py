ea5f3b905916119e2794da783cd11a92c06edfec
Elvis Chege <elvischege@icloud.com>
2022-06-26 06:43:45 +0200

    Token Schema

---
+++ app/schemas/token.py
@@ -0,0 +1,11 @@
+from typing import Union
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
