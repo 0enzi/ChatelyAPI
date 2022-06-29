a638319407698a7600de4d31d3db43b5f82403ca
Elvis Chege <elvischege@icloud.com>
2022-06-29 16:20:03 +0200

    preparing to save socket data to db

--- app/database/session.py
+++ app/database/session.py
@@ -3,5 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core import settings
 
+
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
