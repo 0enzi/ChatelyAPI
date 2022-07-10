b4ae5a84588b9a48ac9c7c882a189b044f871301
Elvis Chege <elvischege@icloud.com>
2022-07-11 01:11:18 +0200

    Old chat w/Postgres is now deprecated :)

--- app/api/v1/chat/chat_ deprecated.py
+++ app/api/v1/chat/chat_ deprecated.py
@@ -1,4 +1,13 @@
+'''     _                               _           _
+  __| | ___ _ __  _ __ ___  ___ __ _| |_ ___  __| |
+ / _` |/ _ \ '_ \| '__/ _ \/ __/ _` | __/ _ \/ _` |
+| (_| |  __/ |_) | | |  __/ (_| (_| | ||  __/ (_| |
+ \__,_|\___| .__/|_|  \___|\___\__,_|\__\___|\__,_|
+           |_|
 
+ Feel free to delete :D
+
+'''
 from typing import List, Any
 from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException, status
 from sqlalchemy.orm import Session
