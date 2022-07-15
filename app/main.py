d65a272a7bf5e90714f1f23e815a6d04950276b9
Elvis Chege <elvischege@icloud.com>
2022-07-15 16:30:39 +0200

    remove reduntant message model

--- app/main.py
+++ app/main.py
@@ -15,8 +15,7 @@ app = FastAPI(title=settings.PROJECT_NAME)
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
-user.Base.metadata.create_all(bind=session.engine)
-message.Base.metadata.create_all(bind=session.engine)
+user.Base.metadata.create_all(bind=session.engine)     
 inbox.Base.metadata.create_all(bind=session.engine)
 
 origins = [
