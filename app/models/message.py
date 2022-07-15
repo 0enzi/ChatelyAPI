2ce6d7f421cc2bf354600e9f447e3947fa73614e
Elvis Chege <elvischege@icloud.com>
2022-07-15 06:34:25 +0200

    message model unusable

--- app/models/message.py
+++ app/models/message.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Boolean, Column, DateTime, Integer, String, Float
-from app.database.base_class import Base
+# from sqlalchemy import Boolean, Column, DateTime, Integer, String, Float
+# from app.database.base_class import Base
 
-class Message(Base):
-    __tablename__ = "message"
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    sender_id = Column(Integer, nullable=False)
-    recipient_id = Column(Integer, nullable=False)
-    msg = Column(String, nullable=False)
-    timestamp = Column(String, nullable=False)
-    read = Column(Boolean, default=False)
+# class Message(Base):
+#     __tablename__ = "message"
+#     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+#     sender_id = Column(Integer, nullable=False)
+#     recipient_id = Column(Integer, nullable=False)
+#     msg = Column(String, nullable=False)
+#     timestamp = Column(String, nullable=False)
+#     read = Column(Boolean, default=False)
