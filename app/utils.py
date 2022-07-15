6c1fbd4e704036d0df74c8f91848f8ba860323ba
Elvis Chege <elvischege@icloud.com>
2022-07-16 00:55:09 +0200

    expire date

--- app/utils.py
+++ app/utils.py
@@ -11,7 +11,7 @@ from app.models.user import User as UserModel
 from app.schemas.token import TokenData
 from sqlalchemy.orm import Session
 
-ACCESS_TOKEN_EXPIRE_MINUTES = 3000  # 30 minutes
+ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # 7 days
 ALGORITHM = "HS256"
 
